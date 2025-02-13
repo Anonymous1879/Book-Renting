1f7282a71ec000902ec8fea9747438d516d09388
Joyash Shrestha <59286326+Anonymous1879@users.noreply.github.com>
2025-02-13 20:17:54 +0545

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'django-insecure-st7w^ea&q#!kc)so6e4k71&9v!awj7g=zr4(!e@w%lmwaqgv1=')
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
@@ -92,7 +92,7 @@ DATABASES = {
 }
 
 # MongoDB connection
-MONGODB_HOST = 'mongodb+srv://test123:test123@cluster0.0qax0.mongodb.net/book_rental_db?retryWrites=true&w=majority'
+MONGODB_HOST = 'MONGO_URI'
 
 # Connect to MongoDB
 mongoengine.connect(host=MONGODB_HOST)
